777eda757c93150b91504dfcf0416fb4991fa0bf
shinkeonkim <singun11@kookmin.ac.kr>
2022-04-19 23:49:20 +0900

    fix: Uncaught ReferenceError: Cannot access 'xakify' before initialization

--- index.js
+++ index.js
@@ -4,25 +4,21 @@
  * MIT Licensed
  */
 
-module.exports = xakify;
-
 const DEFAULT_MASKING_REGEX = /[a-zA-Z0-9ㄱ-힣]/g;
 const DEFAULT_MASKING_TEXT = 'X';
 
-const maskifyContent = (element, maskingText = DEFAULT_MASKING_TEXT, maskifyRegex = DEFAULT_MASKING_REGEX) => {
+const xakify = (element, maskingText = DEFAULT_MASKING_TEXT, maskifyRegex = DEFAULT_MASKING_REGEX) => {
   if(element.tagName === 'SCRIPT' || element.tagName == 'NOSCRIPT') return;
 
   if(element.hasChildNodes()) {
     element.childNodes.forEach((child) => {
-      maskifyContent(child);
+      xakify(child, maskingText, maskifyRegex);
     })
   }
 
-  if(element.nodeName === '#text') {
+  if(element.nodeName === '#text') {    
     element.data = element.data && element.data.replaceAll(maskifyRegex, maskingText);
   }
 }
 
-const xakify = (element, maskingText, maskifyRegex) => {
-  maskifyContent(element, maskingText, maskifyRegex);
-}
+module.exports = xakify;
